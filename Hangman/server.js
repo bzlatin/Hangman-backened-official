5c24d5bc64a97413695a271e6484dccb594e9cd9
bzlatin <btzlatin@gmail.com>
2021-09-21 22:39:46 -0400

    Converts chosen word to character array

--- Hangman/server.js
+++ Hangman/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+app.use(express.json());
 
+var cors = require("cors");
+const port = process.env.port || 8080;
+app.use(cors());
+
+let guess = "";
 const availableWords = [
   "poopy",
   "africa",
@@ -16,12 +21,24 @@ const availableWords = [
   "erect",
 ];
 
-app.get("/", (req, res) => {
-  // Randomly select a word from the availableWords array
-  const randomlyChosenWord = Math.floor(Math.random() * availableWords.length);
+// Randomly select a word from the availableWords array
+const randomlyChosenWord = Math.floor(Math.random() * availableWords.length);
 
-  let currentWord = randomlyChosenWord;
-  res.send(availableWords[currentWord]);
+let currentWord = availableWords[randomlyChosenWord];
+
+// Convert chosen word to character array
+currentWord = currentWord.split("");
+console.log(currentWord);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/usersGuess/:guess", (req, res) => {
+  const p = req.params;
+  guess = p.guess;
+  res.send(`User has guessed the letter: ${guess}`);
+  console.log(guess);
 });
 
 app.listen(port, () => {
